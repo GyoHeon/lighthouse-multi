e072a713c40ec75e7ba227305a5e6c8209237b75
교헌 이 <ghl12078@gmail.com>
2024-01-18 13:43:29 +0900

    Feat: Add menu card

---
+++ src/constants/menu.ts
@@ -0,0 +1,10 @@
+export const MENU = [
+  {
+    url: "https://github.com/GyoHeon/lighthouse-multi",
+    title: "Github",
+  },
+  {
+    url: "https://www.gyoheon.com/resume",
+    title: "Author",
+  },
+];
