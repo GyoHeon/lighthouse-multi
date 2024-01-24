3dbd887f785e9117c32318b01e035a511f2973a9
교헌 이 <ghl12078@gmail.com>
2024-01-24 21:08:55 +0900

    Feat: Add footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,1 +1,10 @@
-export const Footer = () => <footer>This is the footer</footer>;
+export const Footer = () => (
+  <footer className="p-6 border-t">
+    <h4 className="mb-2 text-lg">© 2023 GyoHeon</h4>
+    <p>
+      This is a site for analyzing multiple Google Lighthouse reports at once
+      <br />
+      and comparing the results over time.
+    </p>
+  </footer>
+);
